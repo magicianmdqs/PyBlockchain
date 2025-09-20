699165887c0c8747cd05071feb62d250496b161c
magi <alitabaee2002@gmail.com>
2025-09-20 14:47:11 +0330

    project cleanup

--- blockchain/config.py
+++ blockchain/config.py
@@ -2,7 +2,7 @@ TOTAL_SUPPLY = 1000
 FEE = 0.001  # of the amount
 TAX = 0.01  # of the amount
 INITIAL_SYSTEM_BALANCE = 1012
-PENDING_POOL_LIMIT = 5
+PENDING_POOL_LIMIT = 5 #batch size of every block
 TEST_AUTO_GEN = 1
 '''
 TX Status:
